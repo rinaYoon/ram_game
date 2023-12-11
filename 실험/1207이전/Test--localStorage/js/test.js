6f94b16d5348e68566bf3d546f24e25648f584d9
rinaYoon <ynarina@cttd.co.kr>
2023-12-11 10:58:23 +0900

    폴더링

---
+++ 실험/1207이전/Test--localStorage/js/test.js
@@ -0,0 +1,4 @@
+
+
+var name1 = localStorage.setItem("이름", "깨랑까락1");
+var name2 = localStorage.setItem("이름2", "깨랑까락2");
---
+++ 실험/1207이전/Test--localStorage/js/test2.js
@@ -0,0 +1,9 @@
+
+
+// console.log(localStorage.getItem("이름"));
+// console.log(localStorage.getItem("이름2"));
+
+// console.log(name1); // undefined
+// console.log(name2); // undefined
+
+console.log(localStorage.getItem("새로운이름"));
